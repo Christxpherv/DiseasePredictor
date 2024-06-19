d73459746b696371fe4ba7899e41c8f029a8cc25
Christopher Valle <christxpherv@gmail.com>
2024-06-19 11:52:06 -0500

    use a label encoder to convert the prognosis column to the numerical data type

--- main.py
+++ main.py
@@ -26,3 +26,8 @@ plt.figure(figsize = (18,8))
 sns.barplot(x = "Disease", y = "Counts", data = temp_df)
 plt.xticks(rotation=90)
 plt.show()
+
+
+# encoding the target value into numerical value using LabelEncoder
+encoder = LabelEncoder()
+data["prognosis"] = encoder.fit_transform(data["prognosis"])
