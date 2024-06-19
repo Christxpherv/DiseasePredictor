8371c6d488b0042ee915ab61b137d91c51f7d091
Christopher Valle <christxpherv@gmail.com>
2024-06-19 09:26:44 -0500

    read and check if the dataset we are using is balanced

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+# read the train.csv by removing the last column since it's an empty column
+DATA_PATH = "data/Training.csv"
+data = pd.read_csv(DATA_PATH).dropna(axis = 1)
+
+# checking whether the dataset is balanced or not
+disease_counts = data["prognosis"].value_counts()
+temp_df = pd.DataFrame({
+	"Disease": disease_counts.index,
+	"Counts": disease_counts.values
+})
+
+plt.figure(figsize = (18,8))
+sns.barplot(x = "Disease", y = "Counts", data = temp_df)
+plt.xticks(rotation=90)
+plt.show()
