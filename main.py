7fe90e1aae186206ff127bf4cc9542e9d687cdb7
Christopher Valle <christxpherv@gmail.com>
2024-06-19 16:46:17 -0500

    define scoring metric for k-fold to evaluate the machine learning models; initialize the models (SVD, GB, and RF)

--- main.py
+++ main.py
@@ -40,3 +40,26 @@ X, y, test_size = 0.2, random_state = 24)
 
 print(f"Train: {X_train.shape}, {y_train.shape}")
 print(f"Test: {X_test.shape}, {y_test.shape}")
+
+# defining scoring metric for k-fold cross validation
+def cv_scoring(estimator, X, y):
+	return accuracy_score(y, estimator.predict(X))
+
+# initializing Models
+models = {
+	"SVC":SVC(),
+	"Gaussian NB":GaussianNB(),
+	"Random Forest":RandomForestClassifier(random_state=18)
+}
+
+# producing cross validation score for the models
+for model_name in models:
+	model = models[model_name]
+	scores = cross_val_score(model, X, y, cv = 10, 
+							n_jobs = -1, 
+							scoring = cv_scoring)
+	print("=="*30)
+	print(model_name)
+	print(f"Scores: {scores}")
+	print(f"Mean Score: {np.mean(scores)}")
+
