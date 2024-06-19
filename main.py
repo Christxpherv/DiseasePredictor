05e27c03bb8b1b8181b740cc8922bdccd8710820
Christopher Valle <christxpherv@gmail.com>
2024-06-19 16:32:28 -0500

    split the data to train and test the model into an 80/20 format

--- main.py
+++ main.py
@@ -31,3 +31,12 @@ plt.show()
 # encoding the target value into numerical value using LabelEncoder
 encoder = LabelEncoder()
 data["prognosis"] = encoder.fit_transform(data["prognosis"])
+
+
+X = data.iloc[:,:-1]
+y = data.iloc[:, -1]
+X_train, X_test, y_train, y_test =train_test_split(
+X, y, test_size = 0.2, random_state = 24)
+
+print(f"Train: {X_train.shape}, {y_train.shape}")
+print(f"Test: {X_test.shape}, {y_test.shape}")
