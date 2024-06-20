51d85b49518eaf8c4d0478fe627a510e1b6672d5
Christopher Valle <christxpherv@gmail.com>
2024-06-19 19:45:34 -0500

    started working on a GUI for the code; initialized Tkinter

---
+++ GUI.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from functools import partial
+from main import predictDisease # import the function from main.py
+
+# initialize Tkinter
+root = tk.Tk()
+root.title("Disease Predictor")
